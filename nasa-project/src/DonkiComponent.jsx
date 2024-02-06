25006ae0660520f76100a6d0e0a7fa02e69aa95c
sheikahamed123 <sheikahamed134@gmail.com>
2024-02-06 05:49:54 -0800

    update

--- nasa-project/src/DonkiComponent.jsx
+++ nasa-project/src/DonkiComponent.jsx
@@ -23,7 +23,8 @@ function DonkiComponent({ service, startDate, endDate }) {
 
   return (
     <div>
-      <h2>{service} Data</h2>
+      <h2>{service} Coronal Mass ejection Analysis</h2>
+      <h3>The Space Weather Database Of Notifications, Knowledge, Information (DONKI) is a comprehensive on-line tool for space weather forecasters, scientists, and the general space science community. DONKI chronicles the daily interpretations of space weather observations, analysis, models, forecasts, and notifications provided by the Space Weather Research Center (SWRC), comprehensive knowledge-base search functionality to support anomaly resolution and space science research, intelligent linkages, relationships, cause-and-effects between space weather activities and comprehensive webservice API access to information stored in DONKI.</h3>
       {donkiData && donkiData.length > 0 ? (
         <div>
           {/* Render CME details when CME data is available */}
